80851fe84eb893714386a68ea31f399712bfed81
WindQuintet <122838563+WindQuintet@users.noreply.github.com>
2023-01-16 18:06:36 -0500

    html, js, css

---
+++ index.js
@@ -0,0 +1,13 @@
+red = [0, 100, 63];
+green = [87, 98, 28];
+
+letterColors = [red, green, white];
+
+mouseResponseThreshold = 500
+friction = 0.55
+rotationForce = 0.500
+
+message = 'Welcome!';
+
+drawName(message, letterColors);
+bounceBubbles();
